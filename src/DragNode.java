d10b2045743ff0cff6d7967c803f9925cfc10de3
Ani <81329162+Anidragon@users.noreply.github.com>
2023-07-10 12:39:54 -0400

    added features
    added display feature to show values, and level of nodes. Added value and level of nodes. Debugged some dragging errors

--- src/DragNode.java
+++ src/DragNode.java
@@ -3,19 +3,32 @@ import java.util.ArrayList;
 
 public class DragNode {
     private String name;
+    private String value;
     private int x;
     private int y;
     private Color col;
+    private String display;
+    private int level;
+    private ArrayList<DragNode> children;
 
     public DragNode(String name, int x, int y) {
         this.name = name;
         this.x = x;
         this.y = y;
         this.col = Color.white;
+        this.value = "0";
+        this.display = "names";
+        this.level = 0;
+        children = new ArrayList<DragNode>();
     }
 
     public String getName() {  
         return name;
+    }
+    
+    public String getValue()
+    {
+    	return value;
     }
     
     public Color getColor() {
@@ -29,7 +42,21 @@ public class DragNode {
     public int getY() {
         return y;
     }
-
+    
+    public String getDisplay() {
+    	return display;
+    }
+    
+    public int getLevel()
+    {
+    	return level;
+    }
+    
+    public ArrayList<DragNode> getChildren()
+    {
+    	return children;
+    }
+    
     public void setX(int x) {
         this.x = x;
     }
@@ -46,4 +73,31 @@ public class DragNode {
     {
     	this.name = name;
     }
+    
+    public void setValue(String val)
+    {
+    	value = val;
+    }
+    
+    public void setDisplay(String disp)
+    {
+    	display = disp;
+    }
+    public void setLevel(int lev)
+    {
+    	level = lev;
+    }
+    
+    public void unLevel ()
+    {
+    	this.setLevel(this.getLevel()-1);
+    	if(this.getChildren().size() > 0)
+    	{
+    		for(DragNode child: this.getChildren())
+    		{
+    			child.unLevel();
+    		}
+    	}
+    }
+    
 }
--- src/GraphMain.java
+++ src/GraphMain.java
@@ -11,293 +11,65 @@ import javax.swing.*;
 
 
 
-class GraphPanel extends JPanel {
-    private java.util.List<DragNode> nodes;
-    private java.util.List<LineComp> lines;
-    private DragNode selectedNode;
-    private Point dragOffset;
-    //pop-up menu for editing Nodes
-    public JPopupMenu menu = new JPopupMenu("Menu");
-	public JMenuItem c = new JMenuItem("Color");
-	public JMenuItem r = new JMenuItem("Rename");
-	public JMenuItem aN = new JMenuItem("Add Child Node");
-	public JMenuItem dC = new JMenuItem("Delete Connection");
-	public JMenuItem dN = new JMenuItem("Delete This Node");
 
-    public GraphPanel() {
-    
-    	//list of all nodes and lines currently on the Workspace
-        nodes = new java.util.ArrayList<>();
-        
-        lines = new java.util.ArrayList<>();
-       
-        //options in the pop-up
-        menu.add(r);
-        menu.add(c);
-        menu.add(aN);
-        menu.add(dC);
-        menu.add(dN);
-        
-        //renaming a node
-        r.addActionListener(new ActionListener() { 
-        	  public void actionPerformed(ActionEvent e) { 
-        	    
-        		  menu.setVisible(false);
-	
-        		  String name = selectedNode.getName();
-        		  name = JOptionPane.showInputDialog("rename the node:", selectedNode.getName());
-        		  selectedNode.setName(name);
-        		  
-        		  if(name != null)
-        		  repaint(); 
-	
-        	  } 
-        	} );
-        
-        
-        
-        //re-coloring a nodes
-        c.addActionListener(new ActionListener() { 
-      	  public void actionPerformed(ActionEvent e) { 
-      	    
-      		  menu.setVisible(false);
-      		  //option colors for drop down menu
-      		  String[] options = {"White", "Blue", "Green", "Yellow", "Magenta", "Gray"};
-      		  DragNode node = selectedNode;
-      		  String selection = (String) JOptionPane.showInputDialog(null, "Choose color", "Menu", JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
-      		  //change color based on choice
-      		  switch(selection)
-      		  {
-      		   case "White" : 
-      			   
-      			   selectedNode.setCol(Color.white);
-      			   break;
-      			   
-      		   case "Blue" :
-      			 selectedNode.setCol(Color.blue);
-    			   break;
-      			 
-      		   case "Green" :
-      			 selectedNode.setCol(Color.green);
-    			   break;
-      			
-      		   case "Yellow" :
-      			 selectedNode.setCol(Color.yellow);
-    			   break;
-      			
-      		   case "Magenta" :
-      			 selectedNode.setCol(Color.magenta);
-    			   break;
-      			
-      		   case "Gray" :
-      			 selectedNode.setCol(Color.gray);
-    			   break;
-      		  
-      		  }
-      		  
-      		  repaint(); 
-	
-      	  } 
-      	} );
-        
-        aN.addActionListener(new ActionListener() { 
-      	  public void actionPerformed(ActionEvent e) { 
-      	    
-      		  String name = "";
-      		  name = JOptionPane.showInputDialog("name for new node:");
-      		  
-      		  if(name != null)
-      		  addNode(selectedNode, name, 100, 100);
-      		  repaint(); 
-	
-      	  } 
-      	} );
-        
-        //Deleting Node:
-        dN.addActionListener(new ActionListener() { 
-      	  public void actionPerformed(ActionEvent e) { 
-      	    
-      		  menu.setVisible(false);
-      		  int choice = JOptionPane.showConfirmDialog(null, "Are you Sure you want to delete " + selectedNode.getName() + " ?", "Confirm Delete", JOptionPane.YES_NO_OPTION);
-      		  
-      		  if(choice == 0)
-      		  {
-      		   for(int i = 0; i < lines.size(); i++)
-      		   {
-      			   if(lines.get(i).getStartNode().equals(selectedNode) || lines.get(i).getEndNode().equals(selectedNode))
-      			   {
-      				   lines.remove(i);
-      				   i--;
-      			   }
-      		   }
-      		   nodes.remove(selectedNode);
-      		   repaint(); 
-      		  }
-      		  
-	
-      	  } 
-      	} );
-        
-        addMouseListener(new MouseAdapter() {
-            public void mousePressed(MouseEvent e) {
-                selectNode(e.getX(), e.getY());
-            }
-
-            public void mouseReleased(MouseEvent e) {
-                selectedNode = null;
-            }
-            
-            public void mouseClicked(MouseEvent e)
-            {
-            	selectNode(e.getX(), e.getY());
-            	if(e.getButton() == MouseEvent.BUTTON1 || e.getButton() == MouseEvent.BUTTON2)
-            	{
-            		menu.setVisible(false);
-            	}
-            	
-            	if(selectedNode != null) {
-            
-            	if(e.getButton() == MouseEvent.BUTTON3)
-            	{
-            		
-                            menu.show(null, selectedNode.getX(), selectedNode.getY());
-            		
-            			}
-            		}
-            		
-            	}
-        });
-
-        
-        
-        addMouseMotionListener(new MouseAdapter() {
-            public void mouseDragged(MouseEvent e) {
-                if (selectedNode != null) {
-                    int newX = e.getX() - dragOffset.x;
-                    int newY = e.getY() - dragOffset.y;
-                    selectedNode.setX(newX);
-                    selectedNode.setY(newY);
-                    
-                    for(LineComp line : lines)
-                    {
-                    	if(line.getStartNode().equals(selectedNode))
-                    	{
-                    		line.setStX(selectedNode.getX() + 25);
-                    		line.setStY(selectedNode.getY() + 50);
-                    		break;
-                    	}
-                    	
-                    	else if(line.getEndNode().equals(selectedNode))
-                    	{
-                    		line.setEndX(selectedNode.getX() + 25);
-                    		line.setEndY(selectedNode.getY());
-                    		break;
-                    	}
-                    }
-                    repaint();
-                }
-            }
-        });
-    }
-
-    public void addNode(DragNode startNode, String name, int x, int y) {
-        nodes.add(new DragNode(name, x, y));
-        lines.add(new LineComp("", startNode.getX() + 25, startNode.getY() + 50, nodes.get(nodes.size()-1).getX() + 25, nodes.get(nodes.size()-1).getY()));
-        lines.get(lines.size()-1).setEndNode(nodes.get(nodes.size()-1));
-        lines.get(lines.size()-1).setStartNode(startNode);
-        repaint();
-    }
-    
-    public void addNode(String name, int x, int y) {
-        nodes.add(new DragNode(name, x, y));
-        repaint();
-    }
-
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D) g;
-       
-     
-        for (DragNode node : nodes) {
-            int x = node.getX();
-            int y = node.getY();
-            g2.setColor(node.getColor());
-            g2.fillOval(x, y, 50, 50);
-            g2.setColor(Color.BLACK);
-            g2.drawOval(x, y, 50, 50);
-            g2.drawString(node.getName(), x + 20, y + 30);
-        
-        }
-        
-       
-        for (LineComp line : lines){
-        g2.setColor(line.getColor());
-        g2.drawLine(line.getStX(), line.getStY(), line.getEndX(), line.getEndY());
-        }
-        
-    }
-    
- 
-
-    private void selectNode(int x, int y) {
-        for (DragNode node : nodes) {
-            int nodeX = node.getX();
-            int nodeY = node.getY();
-            if (x >= nodeX && x <= nodeX + 50 && y >= nodeY && y <= nodeY + 50) {
-                selectedNode = node;
-                dragOffset = new Point(x - nodeX, y - nodeY);
-                break;
-            }
-            
-            else
-            {
-            	selectedNode = null;
-            }
-        }
-    }
-}
 
 public class GraphMain {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Graph Visualization");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        final GraphPanel graphPanel = new GraphPanel();
+        final BinaryPanel binPanel = new BinaryPanel();
         JMenuBar bar = new JMenuBar();
         JMenu options = new JMenu("Actions");
         JMenuItem help = new JMenuItem("Help");
         JMenuItem addN = new JMenuItem("Add new Node");
+        
         addN.addActionListener(new ActionListener() { 
+      	  public void actionPerformed(ActionEvent e) { 
+
+        		  String name = "";
+        		  name = JOptionPane.showInputDialog("name for new node:");
+        		  
+        		  if(name != null)
+        		  binPanel.addNode(name, 100, 100);
+        		  binPanel.repaint(); 
+      	  } 
+      	} );
+      
+        JMenuItem disp = new JMenuItem("Change Display");
+        disp.addActionListener(new ActionListener() { 
         	  public void actionPerformed(ActionEvent e) { 
 
-          		  String name = "";
-          		  name = JOptionPane.showInputDialog("name for new node:");
-          		  
-          		  if(name != null)
-          		  graphPanel.addNode(name, 100, 100);
-          		  graphPanel.repaint(); 
+          		  String disp = "";
+          		  String[] options = {"names", "values", "levels"};
+          		  disp = (String) JOptionPane.showInputDialog(null, "Choose Display Type", "Menu", JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+          		  //change color based on choice
+          		  binPanel.changeDisplay(disp);
         	  } 
         	} );
         
+       
         JMenuItem connect = new JMenuItem("Add a connection");
         JMenuItem delConnect = new JMenuItem("Remove existing connection");
     	options.add(help);
     	options.add(addN);
     	options.add(connect);
     	options.add(delConnect);
-    	
+    	options.add(disp);
     	
     	bar.add(options);
     	frame.setJMenuBar(bar);
     	options.setVisible(true);
     	
-        frame.add(graphPanel);
+        frame.add(binPanel);
         
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
      
         frame.setVisible(true); 
 
-        graphPanel.addNode("Node 1", 200, 200);
+        binPanel.addNode("Node 1", 200, 200);
+        binPanel.getNodeList().get(0).setLevel(0);
+        
 
     }
 	
